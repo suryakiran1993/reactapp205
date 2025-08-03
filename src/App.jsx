6fe09c8e16b1e9900a325fb2a12fa77cf0c084a6
Jonnalagadda Surya Kiran <kiransurya93@kluniversity.in>
2025-08-04 01:15:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './style.css';
 function App() {
   return (
     <div className="container">
-      <h1>Welcome to My React Project - S205</h1>
+      <h1>Welcome to My React Project - JSK</h1>
       <p>CI/CD and Cloud DevOps</p>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
